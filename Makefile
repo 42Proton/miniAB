efe542d49184ef1f00b624ae9b49873bd63414bd
Abueskander <basselawni1@gmail.com>
2025-02-03 09:56:16 +0300

    Initialaized the proejct

---
+++ Makefile
@@ -0,0 +1,31 @@
+include Includes.mk
+CC = cc 
+LINKERS= -L./libft/ -lft -L./minilibx-linux/ -lmlx_Linux -lmlx -lXext -lX11 -lm
+CFLAGS = -Wall -Werror -Werror -I./includes/ -I./libft/includes/ -I./minilibx-linux/
+SRC_DIR = src
+OBJ_DIR = build
+NAME = miniRT 
+SRCSDIR = $(addprefix src/,$(SRCS))
+OBJS = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCSDIR)) 
+LIBFT= libft
+MINILIB= minilibx-linux
+
+all: libft minilibx-linux $(NAME)
+
+libft:
+	@make -C $(LIBFT)
+minilibx-linux:
+	@make -C $(MINILIB)
+$(NAME):$(OBJS)
+	@make -C $(LIBFT)
+	$(CC) $(OBJS) $(CFLAGS) $(LINKERS) -o $(NAME)
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c includes/minirt.h
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+clean:
+	@make -C libft fclean
+	rm -f $(OBJS)
+fclean:clean
+	rm -f $(NAME) 
+re: fclean all
+.PHONY: all clean fclean re libft minilibx-linux
---
+++ includes/minirt.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 09:55:59 by abueskander       #+#    #+#             */
+/*   Updated: 2025/02/03 09:56:03 by abueskander      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+#define MINIRT_H
+
+#include <libft.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mlx.h>
+
+#endif
