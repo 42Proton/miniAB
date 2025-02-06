943dae448e5787c238767a9ed0967a4935f5e67d
Abueskander <basselawni1@gmail.com>
2025-02-06 15:04:25 +0300

    Lmao

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:55:59 by abueskander       #+#    #+#             */
-/*   Updated: 2025/02/03 09:56:03 by abueskander      ###   ########.fr       */
+/*   Updated: 2025/02/05 17:52:03 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mlx.h>
-
+#include <math.h>
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,26 @@
 /*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 09:55:48 by abueskander       #+#    #+#             */
-/*   Updated: 2025/02/03 09:55:57 by abueskander      ###   ########.fr       */
+/*   Updated: 2025/02/05 18:11:02 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
+#define PI 3.14
 int main(void)
 {
+  void *mlx;
+  void *win;
+
+  mlx = mlx_init();
+  win = mlx_new_window(mlx, 1000, 1000, "Lenovo");
+
+  for (double t = 0; t < 2 * PI; t += 0.0001)
+  {
+    mlx_pixel_put(mlx, win, cos(t) * 100 + 500, sin(t) * 100 + 500, 0xFFFFFF);
+  }
+
+  mlx_loop(mlx);
   return 0;
 }
