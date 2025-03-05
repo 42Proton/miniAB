4ac0b90ce72b2f36ba4841da4dff85444cd813e7
Abueskander <basselawni1@gmail.com>
2025-03-05 22:53:46 +0300

    Fixed up cleaner

--- src/cleaner.c
+++ src/cleaner.c
@@ -6,32 +6,27 @@
 /*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 20:37:37 by abueskander       #+#    #+#             */
-/*   Updated: 2025/03/05 21:08:04 by abueskander      ###   ########.fr       */
+/*   Updated: 2025/03/05 22:52:26 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
 
+void	object_cleanup(void *content)
+{
+	if (((t_object *)content)->object)
+		free(((t_object *)content)->object);
+	free(content);
+}
+
 void	cleaner(t_rtptr *rts, char *error)
 {
-	t_list	*tmp;
-
 	if (error)
 		ft_dprintf(2, "%s\n", error);
 	if (rts->img)
 		mlx_delete_image(rts->mlx, rts->img);
 	if (rts->mlx)
 		mlx_terminate(rts->mlx);
-	while (rts->objs)
-	{
-		tmp = rts->objs;
-		rts->objs = rts->objs->next;
-		if (tmp->content)
-		{
-			free(((t_object *)(tmp->content))->object);
-			free(tmp->content);
-		}
-		free(tmp);
-	}
+	ft_lstclear(&rts->objs, object_cleanup);
 	exit(EXIT_SUCCESS);
 }
