da8f8069b8d8e38211b4fddfdf14a2be0be7d211
Abueskander <basselawni1@gmail.com>
2025-03-05 20:52:51 +0300

    fixes

--- src/object_init.c
+++ src/object_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   object_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
+/*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 01:17:49 by abueskander       #+#    #+#             */
-/*   Updated: 2025/03/05 03:06:26 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/03/05 20:43:52 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	*ambient_light(void)
 	if (!al)
 		return (NULL);
 	tmp = ft_strtok(NULL, " \t\b\r\f\v\n");
-	al->ratio = ft_atof(tmp, 0);
+	al->ratio = ft_atof(tmp);
+	printf("%f", al->ratio);
 	tmp = ft_strtok(NULL, " \t\b\r\f\v\n");
 	return (al);
 }
