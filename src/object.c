db3fe699d98315e7b03651037b1a637046cf6a55
Abueskander <basselawni1@gmail.com>
2025-03-05 23:08:03 +0300

    Changed the name in Objectify to it doesn't confuse us

--- src/object.c
+++ src/object.c
@@ -3,39 +3,11 @@
 /*                                                        :::      ::::::::   */
 /*   object.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
+/*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 00:06:15 by abueskander       #+#    #+#             */
-/*   Updated: 2025/03/04 16:07:03 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/03/05 23:05:52 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minirt.h>
-
-// USE mlx_put_pixel with image
-
-// void	sphere_draw(double radius, double res, t_rtptr *rts)
-// // {
-// // 	double i;
-// // 	// double j;
-// // 	t_tuple *pnt = point(WID, HEG, 0);
-// // 	// t_tuple *gravity = vector(0,-1,0);
-// // 	// t_tuple *wind = vector(-1,0,0);
-// // 	t_tuple *vec = tuplenormalize(vector(-1.25, -1.8 * 1.25, 0));
-// // 	t_colors *a = colorinit(-200, -233, 0xFF, 0xFF);
-// // 	//        t_colors *b = colorinit(-200,-233,0xFF,0xFF);
-// // 	//	colors = colorinit(0xFF,0xAA,0xFF,0xAA);
-// // 	i = 0;
-// // 	while (i < 100)
-// // 	{
-// // 		pnt = tupleadd(pnt, vec);
-// // 		// vec = tuplesub(vec,wind);
-// // 		// vec = tuplesub(vec,gravity);
-// // 		mlx_pixel_put(rts->mlx, rts->win, pnt->X, pnt->Y, colorvalue(a));
-// // 		i += res;
-// // 	}
-// // 	//  mlx_put_image_to_window(rts->mlx, rts->win, rts->img_st.img, 0, 0);
-// // 	free(vec);
-// // 	free(a);
-// // 	free(pnt);
-// }
--- src/objectify.c
+++ src/objectify.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   objectify.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
+/*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 11:33:42 by abueskander       #+#    #+#             */
-/*   Updated: 2025/03/05 02:56:59 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/03/05 23:07:15 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 t_object	*objectify(int type)
 {
-	t_object *object;
+	t_object	*item;
 
-	object = ft_calloc(1, sizeof(t_object));
-	if (!object)
+	item = ft_calloc(1, sizeof(t_object));
+	if (!item)
 		return (NULL);
 	if (type == AMBIENTLIGHT)
-		object->object = ambient_light();
-	return (object);
+		item->object = ambient_light();
+	return (item);
 }
