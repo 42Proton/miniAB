cb63c541d4f94084accf8b4b20a38a23611b1bc3
stacks-of-data <coder64bit@gmail.com>
2025-03-20 03:12:38 +0300

    norm fix

--- matrix/transformation.c
+++ matrix/transformation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   transformation.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
+/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:56:13 by abueskander       #+#    #+#             */
-/*   Updated: 2025/03/17 03:52:03 by abueskander      ###   ########.fr       */
+/*   Updated: 2025/03/20 03:11:35 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ t_tuple         transform_f(t_matrix *tmatrix, t_tuple *vec)
 {
         t_tuple res;
 
+        ft_bzero(&res, sizeof(t_tuple));
         if(!tmatrix || !vec || vec->w == VECTOR)
-                return ((t_tuple){0, 0, 0, 0});
+                return (res);
         else if (tmatrix->cols != 3 || tmatrix->rows != 3)
-                return ((t_tuple){0, 0, 0, 0});
+                return (res);
         res.x = vec->x * tmatrix->data[0] + vec->y * tmatrix->data[1] + vec->z * tmatrix->data[2];
         res.y = vec->x * tmatrix->data[3] + vec->y * tmatrix->data[4] + vec->z * tmatrix->data[5];
         res.z = vec->x * tmatrix->data[6] + vec->y * tmatrix->data[7] + vec->z * tmatrix->data[8];
