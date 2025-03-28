22969ffaeee6d9443c46ea5c318fe51dbdd0d8ba
stacks-of-data <coder64bit@gmail.com>
2025-03-28 11:03:18 +0300

    optimization

--- rays/normal_vec.c
+++ rays/normal_vec.c
@@ -6,7 +6,7 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 09:16:30 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/03/28 10:57:06 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/03/28 11:02:59 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,12 @@ t_tuple	*sphere_normal(t_sphere *obj, t_tuple *p)
 {
 	t_tuple		object_norm;
 	t_tuple		*world_norm;
-	t_matrix	*t_transpose;
 
 	object_norm = s_tuplesub(matrix_mult_t(obj->inv_t, p), point(0, 0, 0));
-	t_transpose = matrix_transpose(obj->inv_t);
-	if (!t_transpose)
-		return (0);
 	world_norm = malloc(sizeof(t_tuple));
 	if (!world_norm)
-	{
-		free_matrix(t_transpose);
 		return (0);
-	}
-	*world_norm = matrix_mult_t(t_transpose, &object_norm);
-	free_matrix(t_transpose);
+	*world_norm = matrix_mult_t(obj->tpose_inv_t, &object_norm);
 	*world_norm = tuplenormalize(world_norm);
 	world_norm->w = VECTOR;
 	return (world_norm);
