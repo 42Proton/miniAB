cc1c1592cb6da12d3ee04c9a89bb5804f21d8d9e
Proton <basselawni1@gmail.com>
2025-04-04 15:56:22 +0300

    Plane Intersection

--- rays/plane_intersect.c
+++ rays/plane_intersect.c
@@ -6,7 +6,7 @@
 /*   By: bismail <bismail@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 17:46:45 by bismail           #+#    #+#             */
-/*   Updated: 2025/04/03 12:46:01 by bismail          ###   ########.fr       */
+/*   Updated: 2025/04/04 15:39:23 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,23 @@
 
 float	check_plane_intersect(t_plane *plane, t_ray *ray)
 {
-	t_tuple	plane_to_ray;
-	float	dot;
+	t_tuple	*n;
+	float	denom;
+	t_tuple	p2r;
 	float	t;
+	t_tuple	normpos;
 
-	if (!plane || !ray)
-		return (0);
-	plane_to_ray = n_tuplesub(&ray->origin, plane->pos);
-	dot = tupledot(plane->normal_vector, &ray->direction);
-	if (floatcmp(dot, 0))
-		return (0);
-	t = tupledot(plane->normal_vector, &plane_to_ray) / dot;
-	if (t < 0)
-		return (0);
-	return (t);
+	normpos = tuplenormalize(plane->pos);
+	n = plane->normal_vector;
+	denom = tupledot(n, &ray->direction);
+	if (denom < -EPSILON)
+	{
+		p2r = n_tuplesub(&normpos, &ray->origin);
+		t = tupledot(&p2r, n) / denom;
+		if (t >= 0)
+			return (t);
+	}
+	return (0);
 }
 
 int	plane_intersect(t_intersections *insects, t_object_entry *object,
@@ -43,8 +46,8 @@ int	plane_intersect(t_intersections *insects, t_object_entry *object,
 	plane = (t_plane *)object->object;
 	ray_transform = transform_ray(plane->inv_t, ray);
 	t = check_plane_intersect(plane, &ray_transform);
-	if (t == 0)
-		return (0);
+	if (floatcmp(t, 0))
+		return (1);
 	if (!add_intersection(insects, t, object))
 		return (0);
 	return (1);
