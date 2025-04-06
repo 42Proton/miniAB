4e2f102bb222ba59ec83c36878b99b1c0330d461
Proton <basselawni1@gmail.com>
2025-04-06 04:37:22 +0300

    Fixed the funniest error in Libft
     Credit to ChatGPT Copilot reviewer for this

--- libft/srcs/ft_lst/ft_move_lst.c
+++ libft/srcs/ft_lst/ft_move_lst.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_move_lst.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
+/*   By: bismail <bismail@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 01:11:59 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/03/30 00:04:32 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/04/06 04:31:41 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_move_lst(t_list *node, t_list **new_lst)
 {
-	node->next = 0;
-	node->prev = 0;
 	if (node->next)
 		node->next->prev = 0;
+	node->next = 0;
+	node->prev = 0;
 	ft_lstadd_back(new_lst, node);
 }
