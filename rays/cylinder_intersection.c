c53d6d7c8c826247973530fc45676ef5f2cdd1e2
Proton <basselawni1@gmail.com>
2025-04-16 22:49:31 +0300

    I don't know really what is wrong..

--- rays/cylinder_intersection.c
+++ rays/cylinder_intersection.c
@@ -6,7 +6,7 @@
 /*   By: bismail <bismail@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 14:35:39 by bismail           #+#    #+#             */
-/*   Updated: 2025/04/16 18:01:18 by bismail          ###   ########.fr       */
+/*   Updated: 2025/04/16 22:45:37 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,83 +18,72 @@
 
 // }
 
-float           cylinder_first_part(t_ray *ray)
+float cylinder_first_part(t_ray *ray)
 {
-        float res_ray;
-        t_tuple temp;
-
-        temp = point(0,0,0);
-        res_ray = tupledot(&temp, &ray->direction);
-        res_ray =  1 - powf(res_ray,2);
-        
-        return res_ray;
+    return (ray->direction.x * ray->direction.x + ray->direction.z * ray->direction.z);
 }
 
-float           cylinder_middle_part(t_ray *ray)
-{
-        float   res;
-        t_tuple temp;
-        
-        temp = point(0,0,0);
-        res = 2 * (tupledot(&ray->direction,&ray->origin));
-        res -= tupledot(&temp,&ray->direction) * tupledot(&temp , &ray->origin);
-        
-        return (res);
-}
-float   cylinder_last_part(t_ray *ray , float   radius)
-{
-        float res;
-        t_tuple temp;
 
-        temp = point(0,0,0);
-        res = tupledot(&ray->origin,&ray->origin);
-        res -= powf(tupledot(&temp,&ray->origin),2) - powf(radius,2);
-        
-        return (res);
+float cylinder_middle_part(t_ray *ray)
+{
+    return 2 * (ray->origin.x * ray->direction.x + ray->origin.z * ray->direction.z);
+}
+
+float cylinder_last_part(t_ray *ray, float radius)
+{
+    return (ray->origin.x * ray->origin.x + ray->origin.z * ray->origin.z - radius * radius);
+}
+
+float   check_cylinder_intersect(t_cylinder *cy, t_ray *ray)
+{
+	t_quad_eq	quad;
+	float		upper_root;
+	float		lower_root;
+
+	quad.a = cylinder_first_part(ray);
+	quad.b = cylinder_middle_part(ray);
+	quad.c = cylinder_last_part(ray, (cy->dim / 2));
+	quad.discriminant = (quad.b * quad.b) - 4 * (quad.a * quad.c);
+	if (quad.discriminant < 0)
+		return (-1);
+        else if (quad.discriminant == 0)
+        {
+                return(-(quad.b)/ (2 *quad.a));
+        }
+        else if (quad.discriminant > 0)
+        {
+                upper_root = ((-quad.b) + sqrt(quad.discriminant)) / (2 *quad.a);
+                lower_root= ((-quad.b) - sqrt(quad.discriminant)) / (2 *quad.a);
+                if(upper_root > 0)
+                        return(upper_root);
+                else if (upper_root < 0 && lower_root > 0)
+                        return(lower_root);
+                else
+                        return(-1);
+        }
+	return (-1);
 }
 int	cylinder_intersect(t_intersections *insects, t_object_entry *object, t_ray *ray)
 {
         t_cylinder *cylinder;
         t_ray ray_transform;
-        t_quad_eq quad;
-        // float    count;
-        float   upper_root;
-        float   lower_root;
+        float   res;
+        t_tuple         hit_point;
+
         if(!insects || !object || !ray)
                 return (0);
         cylinder = (t_cylinder *)object->object;
         ray_transform = transform_ray(cylinder->inv_t,ray);
-        quad.a = cylinder_first_part(&ray_transform);
-        quad.b = cylinder_middle_part(&ray_transform);
-        quad.c = cylinder_last_part(&ray_transform,cylinder->dim / 2);
-        quad.discriminant = (quad.b * quad.b) + 4 *(quad.a * quad.c);
-        printf("a %f b %f c%f disc %f \n",quad.a,quad.b,quad.c , quad.discriminant);
-        if(quad.discriminant > EPSILON )
-        {
-                float b_term = quad.b < EPSILON ? -quad.b + sqrt(quad.discriminant): -quad.b - sqrt(quad.discriminant);
-                lower_root = b_term /2 *quad.a;
-                upper_root = (2 * quad.c) /b_term;
-                if(lower_root > upper_root)
-                        {
-                                float tmp = lower_root;
-                                lower_root = upper_root;
-                                upper_root = tmp;
-                        }
-        } else if (quad.discriminant > -EPSILON && quad.discriminant <= EPSILON )
-        {
-                lower_root = -(quad.b / 2* quad.a);
-                upper_root = lower_root;
-                return 1;
-        }
-        else
-        {
-                lower_root = NAN;
-                upper_root = NAN;
-        }
-        printf("%f,%f \n",upper_root,lower_root);
-        if (!add_intersection(insects, upper_root, object) && !add_intersection(insects,lower_root,object))
-                 return (0);
+        res = check_cylinder_intersect(cylinder,&ray_transform);
+        printf("res = %f \n", res);
+        if(res == -1)
+                return (1);
+        tuplesmult(&ray_transform.direction, res);
+        hit_point = n_tupleadd(&ray_transform.origin,&ray_transform.direction );
+        if(hit_point.y < cylinder->height /2.0f || hit_point.y > cylinder->height / 2.0f)
+                return (1);
+        if(!add_intersection(insects,res,object))
+                return(0);
         return(1);
-        
 }
 
