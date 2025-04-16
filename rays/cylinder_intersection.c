69f988572bec9c76bd7c984e00dfe9a1db62329d
Proton <basselawni1@gmail.com>
2025-04-16 23:57:58 +0300

    Some progress, but still  stupid not working.

--- rays/cylinder_intersection.c
+++ rays/cylinder_intersection.c
@@ -6,7 +6,7 @@
 /*   By: bismail <bismail@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 14:35:39 by bismail           #+#    #+#             */
-/*   Updated: 2025/04/16 22:45:37 by bismail          ###   ########.fr       */
+/*   Updated: 2025/04/16 23:56:41 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,56 +34,57 @@ float cylinder_last_part(t_ray *ray, float radius)
     return (ray->origin.x * ray->origin.x + ray->origin.z * ray->origin.z - radius * radius);
 }
 
-float   check_cylinder_intersect(t_cylinder *cy, t_ray *ray)
+t_quad_eq   check_cylinder_intersect(t_cylinder *cy, t_ray *ray)
 {
 	t_quad_eq	quad;
-	float		upper_root;
-	float		lower_root;
 
 	quad.a = cylinder_first_part(ray);
 	quad.b = cylinder_middle_part(ray);
-	quad.c = cylinder_last_part(ray, (cy->dim / 2));
+	quad.c = cylinder_last_part(ray, (1));
 	quad.discriminant = (quad.b * quad.b) - 4 * (quad.a * quad.c);
-	if (quad.discriminant < 0)
-		return (-1);
-        else if (quad.discriminant == 0)
-        {
-                return(-(quad.b)/ (2 *quad.a));
-        }
-        else if (quad.discriminant > 0)
-        {
-                upper_root = ((-quad.b) + sqrt(quad.discriminant)) / (2 *quad.a);
-                lower_root= ((-quad.b) - sqrt(quad.discriminant)) / (2 *quad.a);
-                if(upper_root > 0)
-                        return(upper_root);
-                else if (upper_root < 0 && lower_root > 0)
-                        return(lower_root);
-                else
-                        return(-1);
-        }
-	return (-1);
+        return quad;
+}
+int     check_cap(float t1, float height)
+{
+        if (t1 < -height / 2 || t1 > height / 2)
+                return (0);
+        return (1);
 }
 int	cylinder_intersect(t_intersections *insects, t_object_entry *object, t_ray *ray)
 {
         t_cylinder *cylinder;
         t_ray ray_transform;
-        float   res;
-        t_tuple         hit_point;
+        t_quad_eq       quad;
+        // t_tuple         hit_point;
+        float t1;
+        float t2;
+
 
         if(!insects || !object || !ray)
                 return (0);
         cylinder = (t_cylinder *)object->object;
         ray_transform = transform_ray(cylinder->inv_t,ray);
-        res = check_cylinder_intersect(cylinder,&ray_transform);
-        printf("res = %f \n", res);
-        if(res == -1)
+        quad = check_cylinder_intersect(cylinder,&ray_transform);
+        if(quad.discriminant < 0)
                 return (1);
-        tuplesmult(&ray_transform.direction, res);
-        hit_point = n_tupleadd(&ray_transform.origin,&ray_transform.direction );
-        if(hit_point.y < cylinder->height /2.0f || hit_point.y > cylinder->height / 2.0f)
-                return (1);
-        if(!add_intersection(insects,res,object))
+        if(quad.discriminant == 0)
+        {
+                t1 = ((-quad.b) + quad.discriminant) / (2 * quad.a);
+                if(!check_cap(t1,cylinder->height))
+                        return(1);
+                if(!add_intersection(insects,t1,object))
                 return(0);
+        }else
+        {
+                t1 = ((-quad.b) + quad.discriminant) / (2 * quad.a);
+                t2 = ((-quad.b) - quad.discriminant) / (2 * quad.a);
+                if(check_cap(t1,cylinder->height))
+                        if(!add_intersection(insects,t1,object))
+                                return(0);
+                if(check_cap(t1,cylinder->height))
+                        if(!add_intersection(insects,t2,object))
+                                return(0);
+        }
         return(1);
 }
 
